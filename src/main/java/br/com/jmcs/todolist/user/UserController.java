e06f83bb80127c3baee7872abfccdacbe580143f
JulianaMCS <julyanamcs@gmail.com>
2024-06-27 22:54:28 -0300

    utilizando responseEntity

--- src/main/java/br/com/jmcs/todolist/user/UserController.java
+++ src/main/java/br/com/jmcs/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package br.com.jmcs.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,16 +34,18 @@ private IUserRepository userRepository;
  */
 
  @PostMapping("/user")
-    public UserModel create(@RequestBody UserModel usermodel){
+    public ResponseEntity create(@RequestBody UserModel usermodel){
       var user = this.userRepository.findByUsername(usermodel.getUsername());
 
       if(user != null){
-         System.err.println("Usuario já existe");
-         return null;
+        
+         //Mensagem de erro 
+         //Status code
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
       }
       
        var userCreated = this.userRepository.save(usermodel);
-       return userCreated;
+       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
